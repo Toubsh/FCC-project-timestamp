39853c7880f69823fdb4683aa7626f0e4ea22533
Toubsh <Toubsh@hotmail.de>
2021-01-10 13:39:37 +0100

    Write backend for project to pass

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -24,7 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp/:date?", function(req, res) {
+  if (req.params.date === '' || req.params.date === undefined || req.params.date === null) {
+    res.json({ unix: Math.floor(new Date().getTime()), utc : new Date()  });
+  }
+  if (new Date(req.params.date) == "Invalid Date" && new Date(req.params.date / 1000) == "Invalid Date") {
+    res.json({ error : "Invalid Date" });
+  }
+  let date;
+  if (req.params.date / 1000) {
+    date = new Date(parseInt(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
